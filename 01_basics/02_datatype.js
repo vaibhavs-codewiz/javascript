03843df08f33f66ab34b55fe691360735cc1b787
vaibhavs-codewiz <159580035+vaibhavs-codewiz@users.noreply.github.com>
2024-04-17 16:29:53 +0000

    data types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer verison
+
+//alert( 3 + 3); //we are using node js not browser have different syntax
+
+console.log(4 + 3);
+console.log("vaibhav"); // maintain the code redability
+// data types in JS
+let age = 20;
+let name = "vaibhav";
+let state; // undefined since value not assigned
+
+//number = range2^53
+//bigint = greater range than number
+//boolean = true/false
+//null = standalone value . it is not a type but an object
+// null value assigned to variable when we cannot say what value should it have
+//and also need not want to remain it undefined
+//undefined = this is also a data type,
+//symbol = to find uniqueness will learn later
+//object
+
+console.log(typeof 234);
+console.log(typeof name);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
