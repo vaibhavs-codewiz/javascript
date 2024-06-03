1424c988a5853015ff43dd37fbb499a837619810
vaibhavs-codewiz <vaibhavsharma000721@gmail.com>
2024-06-04 00:59:47 +0530

    create

--- d3 click svg animation/script.js
+++
@@ -1,4 +0,0 @@
-//create sleak svg animations : SVG images can be scaled to any size without loss of 
-//quality because they are defined using vectors (mathematical expressions) rather 
-//than pixels. SVG stands for (scalable vector graphics.).
-
